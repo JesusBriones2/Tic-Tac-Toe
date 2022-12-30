e1c4566c6fbfe51ae6da1a97c82314b4ab32b21c
“jesusbriones” <leojesusbriones2@gmail.com>
2022-12-30 17:20:02 -0500

    interface completed

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
-document.querySelector('.btn1').addEventListener('click', (e) => {
-    e.target.parentElement.classList.add('menu--hide')
-})
-document.querySelector('.btn2').addEventListener('click', (e) => {
-    e.target.parentElement.classList.add('menu--hide')
-})
-document.querySelector('.btn3').addEventListener('click', () => {
-    document.querySelector('.menu').classList.remove('menu--hide')
-})
+(() => {
+
+    // Controles del menu principal.
+    const menu = document.getElementById('menu');
+    const gameContainer = document.querySelector('.game-container');
+
+    menu.addEventListener('click', (e) => {
+        if (e.target.id == 'cta-computer') {
+            console.log('output1');
+            gameContainer.classList.add('menu--hide');
+        }
+        else if (e.target.id == 'cta-2players') {
+            console.log('output2');
+            gameContainer.classList.add('menu--hide');
+        }
+    });
+
+    document.getElementById('cta-back').addEventListener('click', () => {
+        gameContainer.classList.remove('menu--hide');
+    });
+
+
+})();
+
